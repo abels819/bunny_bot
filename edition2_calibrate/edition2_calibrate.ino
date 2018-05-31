ca8da344a320067119f3d0919ec68f6c05e33dd3
abels819 <244590003@qq.com>
2018-06-01 01:26:06 +0800

    2018.6.1
    added random movements, head movements, prone

--- edition2_calibrate/edition2_calibrate.ino
+++ edition2_calibrate/edition2_calibrate.ino
@@ -23,27 +23,78 @@ int counter = 0;
 
 //motions..............................................................................................................
 void head_check_left() {
+  set(H,30);
+  give_pwm(5);
 }
 
 void head_check_right() {
+  set(H,150);
+  give_pwm(5);
 }
 
 void check_half_left() {
+  set(H,60);
+  give_pwm(5);
 }
 
 void check_half_right() {
+  set(H,120);
+  give_pwm(5);
 }
 
 void head_return() {
+  set(H,90);
+  set(N1,120);
+  set(N2,40);
+  give_pwm(5);
 }
 
 void check_low() {
+  set(N1,90);
+  set(N2,90);
+  give_pwm(5);
 }
 
 void check_high() {
+  set(N1,135);
+  set(N2,40);
+  give_pwm(5);
+}
+
+void random_head_check(int looptimes){
+    
+  randomSeed(analogRead(A1));
+  looptimes=looptimes*(random(3)+1);
+    for(int i=0;i<looptimes;i++){
+      int todo=random(7);
+      int delaytime=random(1000)+200;
+      if(todo==0){
+        head_return();  
+      }  
+      if(todo==1){
+        head_check_left();  
+      }
+      if(todo==2){
+        head_check_right();  
+      }
+      if(todo==3){
+        check_half_left();
+      }
+      if(todo==4){
+        check_half_right();  
+      }
+      if(todo==5){
+        check_low();  
+      }
+      if(todo==6){
+        check_high();  
+      }
+      delay(delaytime);
+    }
 }
 
 void walk_forward() {
+  check_low();
   int LTv = 0;
   int RTv = 0;
   int LFv = 70;
@@ -58,15 +109,15 @@ void walk_forward() {
   set(RN, 70);
   set(LA,5);
   give_pwm(5);
-  for (int i = 0; i < 25; i++) {
-    LTv += 2;
-    RTv += 2;
-    LFv -= 2;
-    RFv -= 2;
-    RSv+=2;
-    LSv-=1;
-    LAv+=1;
-    RAv-=2;
+  for (int i = 0; i < 12; i++) {
+    LTv += 4;
+    RTv += 4;
+    LFv -= 4;
+    RFv -= 4;
+    RSv+=4;
+    LSv-=2;
+    LAv+=2;
+    RAv-=4;
     set(LT, LTv);
     set(RT, RTv);
     set(LF, LFv);
@@ -79,14 +130,14 @@ void walk_forward() {
   }
   set(RA,20);
   give_pwm(5);
-  for (int i = 0; i < 25; i++) {
-    LTv += 2;
-    RTv += 2;
-    LNv+=3;
-    RNv+=2;
-    LSv+=1;
-    RSv-=1;
-    RAv-=4;
+  for (int i = 0; i < 12; i++) {
+    LTv += 4;
+    RTv += 4;
+    LNv+=6;
+    RNv+=4;
+    LSv+=2;
+    RSv-=2;
+    RAv-=8;
     set(LT, LTv);
     set(RT, RTv);
     set(LN, LNv);
@@ -97,7 +148,7 @@ void walk_forward() {
     set(RA, RAv);
     give_pwm(1);
   }
-  set(LT, 90);
+  set(LT, 80);
   set(RT, 90);
   set(LF, 12);
   set(RF, 10);
@@ -105,7 +156,7 @@ void walk_forward() {
   set(RS, 50);
   set(LA, 50);
   set(RA, 90);
-  give_pwm(3);
+  give_pwm(4);
   set(LS, 50);
   set(RS, 50);
   set(LN, 180);
@@ -127,6 +178,7 @@ void walk_forward() {
 }
 
 void move_right() {
+  head_check_right();
   set(LS,90);
   set(LA,30);
 
@@ -139,7 +191,8 @@ void move_right() {
   set(RF,80);
   give_pwm(4);
   
-  set(SPINE,110);
+  for(int i=0;i<10;i++){
+    set(SPINE,110);
   set(LS,30);
   set(RS,30);
   
@@ -149,13 +202,15 @@ void move_right() {
 
   set(RT,10);
   set(RN,80);
-  
-  give_pwm(10);
+  give_pwm(1);
+  delay(20);
+    }
   reset();
   give_pwm(20);
 }
 
 void move_left() {
+  head_check_left();
   set(LS,90);
   set(LA,30);
 
@@ -168,6 +223,7 @@ void move_left() {
   set(RF,80);
   give_pwm(4);
   
+  for(int i=0;i<10;i++){
   set(SPINE,50);
   set(LS,30);
   set(RS,30);
@@ -178,8 +234,9 @@ void move_left() {
 
   set(LT,10);
   set(LN,80);
-  
-  give_pwm(10);
+  give_pwm(1);  
+  delay(20);
+  }
   reset();
   give_pwm(20);
   delay(1000);
@@ -205,6 +262,7 @@ void random_walk(){
 }
 
 void run_forward() {
+  check_low();
   set(LT, 30);
   set(RT, 30);
   set(LF, 50);
@@ -215,23 +273,21 @@ void run_forward() {
   set(RS, 20);
   set(LA, 20);
   set(RA, 40);
-  give_pwm(10);
-  delay(5);
-  set(LT, 90);
+  give_pwm(6);
+  set(LT, 80);
   set(RT, 90);
-  set(LF, 12);
-  set(RF, 10);
+  set(LF, 2);
+  set(RF, 0);
   set(LS, 50);
   set(RS, 50);
   set(LA, 50);
   set(RA, 90);
-  give_pwm(10);
-  delay(5);
+  give_pwm(6);
   reset();
   set(LS, 50);
   set(RS, 50);
-  set(LN, 0);
-  set(RN, 0);
+  set(LN,25);
+  set(RN,25);
   set(LF, 0);
   set(RF, 0);
   give_pwm(4);
@@ -241,13 +297,84 @@ void run_forward() {
   give_pwm(8);
   reset();
   give_pwm(3);
-  delay(1000);
 }
 
 void run_left() {
+  head_check_left();
+  set(LS,90);
+  set(LA,30);
+
+  set(RS,90);
+  set(RA,30);
+  
+  set(LT,15);
+  set(RT,15);
+  set(LF,80);
+  set(RF,80);
+  give_pwm(4);
+  
+  set(SPINE,50);
+  set(LS,30);
+  set(RS,30);
+  
+  set(RT,90);
+  set(RN,30);
+  set(RF,0);
+
+  set(LT,10);
+  set(LN,80);
+  give_pwm(10);
+  reset();
+  give_pwm(20);
+  delay(1000);
 }
 
 void run_right() {
+  head_check_right();
+  set(LS,90);
+  set(LA,30);
+
+  set(RS,90);
+  set(RA,30);
+  
+  set(LT,15);
+  set(RT,15);
+  set(LF,80);
+  set(RF,80);
+  give_pwm(4);
+  
+    set(SPINE,110);
+  set(LS,30);
+  set(RS,30);
+  
+  set(LT,90);
+  set(LN,30);
+  set(LF,0);
+
+  set(RT,10);
+  set(RN,80);
+  give_pwm(10);
+  reset();
+  give_pwm(20);
+}
+
+void random_run(){
+  randomSeed(analogRead(A1));
+  int looptimes=random(3)+1;
+    for(int i=0;i<looptimes;i++){
+      int todo=random(3);
+      int delaytime=random(200)+200;
+      if(todo==0){
+        run_forward();  
+      }  
+      if(todo==1){
+        run_left();  
+      }
+      if(todo==2){
+        run_right();  
+      }
+      delay(delaytime);
+    }
 }
 
 void roll() {
@@ -257,8 +384,31 @@ void stand() {
 }
 
 void prone() {
+  check_low();
+  for(int i=0;i<8;i++){
+      set(LT,80);
+      set(RT,80);
+      set(LN,20);
+      set(RN,20);
+      set(LF,0);
+      set(RF,0);
+      set(LS,130);
+      set(RS,130);
+      set(LA,0);
+      set(RA,0);
+      give_pwm(1);
+      delay(30);
+  }
+  int looptimes=random(2)+1;
+  random_head_check(looptimes);
+  reset();
+  give_pwm(6);
+  delay(500);
 }
 
+void poof(){
+  
+}
 void jump() {
 }
 
@@ -432,6 +582,23 @@ void primary() {
   }
 }
 
-void loop() {
-  random_walk();
+void test_loop() {
+  randomSeed(analogRead(A0));
+  int todo=random(4);
+  if(todo==0){
+    random_run();  
+  }
+  if(todo==1){
+    random_head_check(1);  
+  }
+  if(todo==2){
+    random_walk();  
+  }
+  if(todo==3){
+    prone();  
+  }
+}
+
+void loop(){
+  test_loop();
 }
